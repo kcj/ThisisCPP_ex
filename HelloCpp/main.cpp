a056b4c6997efc52f917b9352486255e223710f7
CJ KIM <cjkim@cjui-macbook-pro.local>
2018-07-28 17:35:32 +0900

    Initial Commit

---
+++ HelloCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloCpp
+//
+//  Created by CJ KIM on 2018. 7. 28..
+//  Copyright © 2018년 CJ KIM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
